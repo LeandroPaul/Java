53799199e3553e76d5acd0679fdf67ded1b4af6f
LeandroPaul <117759006+LeandroPaul@users.noreply.github.com>
2023-03-06 13:48:03 -0300

    revisar

--- Estrutura de Dados/ex2/Exercicio2/src/App.java
+++ Estrutura de Dados/ex2/Exercicio2/src/App.java
@@ -3,14 +3,14 @@ public class App {
         // Faça um programa para apresentar na tela todos os números inteiros entre 100
         // e 1 (ordem decrescente).
         
-        for (int i = 0; i<=100; i++) { 
-            if (i%2 == 0){
-                continue;
-                System.out.println(i);
+        for (int i = 1; i<100; i++) { 
+            int soma=i+1;
+            if(soma%2==0){
+                System.out.println("Posição i: "+(i)+" "+"Número inteiros: "+soma);
+            }
+                
 
         }
-    }
-        
     }
 
 }
