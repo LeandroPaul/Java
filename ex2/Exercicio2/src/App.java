7e83cec01e4eb0a6b4816295409964fe48738231
LeandroPaul <117759006+LeandroPaul@users.noreply.github.com>
2023-03-05 23:41:46 -0300

    revisar

---
+++ ex2/Exercicio2/src/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Faça um programa para apresentar na tela todos os números inteiros entre 100
+        // e 1 (ordem decrescente).
+        
+        for (int i = 0; i<=100; i++) { 
+            if (i%2 == 0){
+                continue;
+                System.out.println(i);
+
+        }
+    }
+        
+    }
+
+}
