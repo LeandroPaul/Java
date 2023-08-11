94d05ccc9d84167c37e265b7047c434a203ecac5
Leandro Paulino <Leandropaulino890@gmail.com>
2023-08-11 13:43:11 -0300

    PRÁTICA

---
+++ EstruturaCondicional/pratica_8/main.java
@@ -0,0 +1,15 @@
+/*Em um país imaginário denominado Lisarb, todos os habitantes ficam felizes em pagar seus impostos, pois sabem que nele não existem políticos corruptos e os recursos arrecadados são utilizados em benefício da população, sem 
+qualquer desvio. A moeda deste país é o Rombus, cujo símbolo é o R$. Leia um valor com duas casas decimais, equivalente ao salário de uma pessoa de Lisarb. Em seguida, calcule e mostre o valor que esta pessoa deve pagar de Imposto de Renda, segundo a tabela abaixo. */
+
+package EstruturaCondicional.pratica_8;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main (String[] args) throws Exception{
+        Scanner sc = new Scanner (System.in);
+        System.out.println("-------------------------------------------------------------------------------------------------");
+        System.out.println("                            LISARB - IMPOSTO DE RENDA\n  PAGUE SEU IMPOSTO DE RENDA EM DIA, TODOS OS RECURSOS ARRECADADOS SÃO BENEFÍCIOS SOCIAIS.\n-------------------------------------------------------------------------------------------------");
+        
+    }
+}
