3b5ff575134a5b49e86bae0d62dadab77183b009
Leandro Paulino <Leandropaulino890@gmail.com>
2023-08-19 22:04:47 -0300

    Estudando

--- EstruturaRepetitiva/pratica_3/main.java
+++ EstruturaRepetitiva/pratica_3/main.java
@@ -1,25 +1,32 @@
-/*Um Posto de combustíveis deseja determinar qual de seus produtos tem a preferência de seus clientes. Escreva 
-um algoritmo para ler o tipo de combustível abastecido (codificado da seguinte forma: 1.Álcool 2.Gasolina 3.Diesel 
-4.Fim). Caso o usuário informe um código inválido (fora da faixa de 1 a 4) deve ser solicitado um novo código (até 
-que seja válido). O programa será encerrado quando o código informado for o número 4. Deve ser escrito a 
-mensagem: "MUITO OBRIGADO" e a quantidade de clientes que abasteceram cada tipo de combustível, conforme 
-exemplo. */
-
 package EstruturaRepetitiva.pratica_3;
+
 import java.util.Scanner;
 public class main {
-    public static void main (String args) throws Exception{
+    public static void main (String[] args) throws Exception{
         Scanner sc = new Scanner(System.in);
-        int alcool,gasolina,diesel,fim;
+        int alcool=0,gasolina=0,diesel=0,fim;
         int codigo=0;
-        int soma = 0;
+        System.out.print("          Posto Lino\nInforme qual dos produtos é de sua maior preferência\n1 - Alcool\n2 - Diesel\n3 - Gasolina\n4 - Encerrar\n");
+        
         while(codigo!=4){
-            System.out.print("Posto Lino\nInforme qual dos produtos é de sua preferência\n1 - Alcool\n2 - Diesel\nDiesel\n4 - Encerrar");
-            System.out.print("Codigo do produto: ");
+            System.out.print("Codigo: ");
             codigo = sc.nextInt();
+            System.out.println("-----------------------------");
             if(codigo==1){
-                soma++;
-                System.out.print("Gasolina: "+soma);
+                alcool++;
+            }
+            else if(codigo==2){
+                diesel++;
+            }
+             else if(codigo==3){
+                gasolina++;
+            }
+            else if(codigo>4){
+                System.out.println("Digite um número entre 1 e 4.");
+                System.out.println("-----------------------------");
+            }
+            if(codigo==4){
+                System.out.printf("MUITO OBRIGADO\nAlcool: %d\nDiesel: %d\nGasolina: %d",alcool,diesel,gasolina);
                 break;
             }
 
